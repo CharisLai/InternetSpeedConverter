4a419a0453df301fb338bd8ffcf3eab01e4e745b
CharisLai <griffin8418@gmail.com>
2023-09-28 14:56:01 +0800

    feat: components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import CardFooter from './components/CardFooter.js';
+import UnitControl from './components/UnitControl.js';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState(0)
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  }
+
+  const UnitConverter = () => (
+    <div className="converter">
+      <div className="flex-1">
+        <div className="converter-title">Set</div>
+        <input type="number" className="input-number" min="0" onChange={handleInputChange} value={inputValue} />
+      </div>
+      <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
+        <i className="fas fa-angle-right" ></i>
+      </span>
+      <div className="text-right flex-1">
+        <div className="converter-title">Show</div>
+        <input type="text" className="input-number text-right" disabled value={inputValue / 8} />
+      </div>
+    </div>
+  )
+
   return (
     <div>
       <div className="container">
         <div className="card-header">Network Speed Converter</div>
         <div className="card-body">
-          <div className="unit-control">
-            <div className="unit">Mbps</div>
-            <span className="exchange-icon fa-fw fa-stack">
-              <i className="far fa-circle fa-stack-2x" ></i>
-              <i className="fas fa-exchange-alt fa-stack-1x" ></i>
-            </span>
-            <div className="unit">MB/s</div>
-          </div>
-          <div className="converter">
-            <div className="flex-1">
-              <div className="converter-title">Set</div>
-              <input type="number" className="input-number" min="0" />
-            </div>
-            <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
-              <i className="fas fa-angle-right" ></i>
-            </span>
-            <div className="text-right flex-1">
-              <div className="converter-title">Show</div>
-              <input type="text" className="input-number text-right" disabled value="125" />
-            </div>
-          </div>
+          <UnitControl />
+          <UnitConverter />
         </div>
-        <div className="card-footer">FAST</div>
+        <CardFooter />
       </div>
     </div>
   )
